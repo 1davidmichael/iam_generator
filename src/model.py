2e93acea9e7d804f994a26255d342a574818f76d
David Michael <1.david.michael@gmail.com>
2022-02-17 22:04:41 -0600

    Remove cruft

--- src/model.py
+++ src/model.py
@@ -1,5 +1,4 @@
 from policy_sentry.util import arns
-from policy_sentry.writing.template import get_actions_template_dict
 from policy_sentry.command.write_policy import write_policy_with_template
 from policy_sentry.writing.template import get_crud_template_dict
 import json
